c6cffb780df4a9f55d85331081e2b405a7ebc611
AppGitUsers <appuser@sssedu.in>
2026-05-14 23:46:31 -0400

    push from the server for deployment configurations

--- call_tracker-backend/call_Tracker/call_Tracker/settings.py
+++ call_tracker-backend/call_Tracker/call_Tracker/settings.py
@@ -19,7 +19,11 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
